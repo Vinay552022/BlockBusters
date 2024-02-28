075b5638c131311ba4ab69722de4c4bd8b69d097
Vinay <vinay552022@gmail.com>
2024-02-28 22:59:06 +0530

    ll

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function FileUpload() {
 
   return (
     <div>
-    <p>hello world</p>
+    <p>hello sravani</p>
       <input type="file" onChange={handleFileChange} />
       <button onClick={handleUpload} disabled={uploading}>
         {uploading ? 'Uploading...' : 'Upload'}
